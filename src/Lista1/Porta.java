dbc6ff1b956eaaa7354ef06b67cc5bc5bc0e707e
ASUS\Cicero <cicero.ferreira@target.work.com.br>
2022-02-21 21:04:19 -0300

    implementado classe porta ex 1.7

--- src/Lista1/Porta.java
+++ src/Lista1/Porta.java
@@ -1,5 +1,22 @@
 package Lista1;
 
+//Ex 1.7 
 public class Porta {
 	
+	Boolean isOpen;
+	int numAberturas;
+	
+	public Porta (){
+
+	}
+	
+	public void abrir(){
+		isOpen = true;
+		numAberturas++;
+	}
+	
+	public int fechar(){
+		isOpen = false;		
+		return numAberturas;
+	}
 }
--- src/Lista1/ProjetoExemplo.java
+++ src/Lista1/ProjetoExemplo.java
@@ -4,7 +4,11 @@ public class ProjetoExemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Porta porta = new Porta();		
+		porta.abrir();
+		
+		Boolean portaAberta = porta.isOpen;
+		System.out.println("resultado = " + portaAberta);
 	}
 
 }
